adee5bfb2c806826754df96a5d14b811ee595d27
Toochidennis <dennistoochukwu@gmail.com>
2023-10-14 13:46:08 +0100

    Adjusted the variables names and added new ones

--- app/src/main/java/com/digitaldream/linkskool/models/UpcomingQuizModel.kt
+++ app/src/main/java/com/digitaldream/linkskool/models/UpcomingQuizModel.kt
@@ -2,7 +2,11 @@ package com.digitaldream.linkskool.models
 
 data class UpcomingQuizModel(
     val id: String,
-    val courseName: String,
     val title: String,
-    val date: String
+    val date: String,
+    val type: String,
+    val courseName: String,
+    val courseId: String,
+    val levelId: String,
+    val term: String,
 )
